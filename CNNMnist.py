218952838534b835797d7be7cd7953ba6a9e524e
aquapathos <aquapathos@users.noreply.github.com>
2017-08-06 14:20:08 +0900

    201708061420

--- CNNMnist.py
+++ CNNMnist.py
@@ -10,9 +10,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 import seaborn as sns
 import tensorflow as tf
 import mytfext
-get_ipython().magic('matplotlib inline')
-
-# ※ tensorflow にはMNISTのサンプルデータをダウンロードするためのモジュールが備わっている
+#  tensorflow にはMNISTのサンプルデータをダウンロードするためのモジュールが備わっている
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 
@@ -142,13 +140,13 @@ class NN:
 
 # # セッションの開始
 
-nn = NN(learning_rate=0.001)
+nn = NN(0.0001,32,64,1024)
 times = 0
-for _ in range(2000):
+for _ in range(200):
     times += 1
     batch_xs,batch_ts = mnist.train.next_batch(30)
     nn.sess.run(nn.train_step, feed_dict={nn.keep_prob:0.5,nn.img:batch_xs, nn.ideal:batch_ts})
-    if times % 100 == 0:
+    if times % 10 == 0:
         summary,loss_val, acc_val = nn.sess.run([nn.summary,nn.loss,nn.accuracy],
                 feed_dict={nn.keep_prob:1.0,nn.img:mnist.test.images, nn.ideal:mnist.test.labels})
         print("Step: {0:d}, Loss: {1:f}, Accuracy: {2:f}".format(times, loss_val, acc_val))
@@ -162,83 +160,3 @@ for _ in range(2000):
 # 普通の隠れ層１のNNや畳み込み層１隠れ層なしのNNでも98％は到達するのだから当然なのかもしれない。
 
 # # 学習結果を用いて文字認識してみる
-
-plt.figure(figsize=(8,15))
-def plotchr(image,label1,label2, i):
-    sns.set_context("talk")
-    plt.subplot(10,6,i)
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("%d / %d" % (np.argmax(label1),np.argmax(label2)))
-    plt.imshow(image,cmap=plt.cm.gray_r)
-
-images, labels = mnist.test.images, mnist.test.labels
-p_val = nn.sess.run(nn.out, feed_dict={nn.img:images, nn.ideal:labels})
-
-# 0～9 と判定されたものについて、それぞれ正の例を３つ、負の例を３つ表示する
-picnumber = 0
-for i in range(10):
-    c = 0
-    for (image, label, pred) in zip(images, labels, p_val):
-        prediction,actual = np.argmax(pred), np.argmax(label)
-        if prediction != i:  #
-            continue
-        if(c<3 and i == actual) or (c>=3 and i != actual):
-            picnumber += 1
-            plotchr(image.reshape((28,28)),pred,label,picnumber)
-            c += 1
-            if c > 5:
-                break
-
-num_filters=16
-
-def showFilterEffect(num_filters, start, filters,imgs):
-    fig = plt.figure(figsize=(10,num_filters+1))
-    for i in range(num_filters):
-        subplot = fig.add_subplot(num_filters+1, 10, 10*(i+1)+1)
-        sns.heatmap(filters[:,:,0,i],yticklabels=[],xticklabels=[],
-                cmap=plt.cm.bwr,cbar=False,annot=False,square=True)
-
-    for i in range(9):
-        subplot = fig.add_subplot(num_filters+1, 10, i+2)
-        oimg = mnist.test.images[start+i].reshape((28,28))
-        title = '{:d}'.format(np.argmax(mnist.test.labels[start+i]))
-        subplot.set_title(title)
-        sns.heatmap(oimg,yticklabels=[],xticklabels=[],
-                cmap=plt.cm.binary,cbar=False,annot=False,square=True)
-
-        for f in range(num_filters):
-            subplot = fig.add_subplot(num_filters+1, 10, 10*(f+1)+i+2)
-            sns.heatmap(imgs[i][:,:,f],yticklabels=[],xticklabels=[],
-                cmap=plt.cm.binary,cbar=False,annot=False,square=True)
-
-filter_vals, conv_vals, pool_vals = nn.sess.run(
-    [nn.W_conv, nn.h_conv, nn.h_pool], feed_dict={nn.img:mnist.test.images[0:10]})
-showFilterEffect(16,0,filter_vals,conv_vals)
-
-showFilterEffect(16,0,filter_vals,pool_vals)
-
-fig = plt.figure(figsize=(12,10))
-c=0
-for i in range(len(mnist.test.images)):
-    image = mnist.test.images[i:i+1]
-    label = mnist.test.labels[i]
-    p_val = nn.sess.run(nn.out, feed_dict={nn.img:image})
-    pred = p_val[0]
-    prediction, actual = np.argmax(pred), np.argmax(label)
-    if prediction == actual:
-        continue
-    subplot = fig.add_subplot(5,4,c*2+1)
-    subplot.set_xticks([])
-    subplot.set_yticks([])
-    subplot.set_title('{} / {}'.format(prediction, actual))
-    subplot.imshow(image.reshape((28,28)), vmin=0, vmax=1,
-                   cmap=plt.cm.binary, interpolation="nearest")
-    subplot = fig.add_subplot(5,4,c*2+2)
-    subplot.set_xticks(range(10))
-    subplot.set_xlim(-0.5,9.5)
-    subplot.set_ylim(0,1)
-    subplot.bar(range(10), pred, align='center')
-    c += 1
-    if c == 10:
-        break
